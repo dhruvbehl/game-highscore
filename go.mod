5f94c6df56987bf1caed67b1db0dc1fc30092ef6
dhruv behl <dhruvbhl@gmail.com>
2022-02-10 05:05:10 +0530

    Adding server interface implemeentation

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/dhruvbehl/game-highscore
 go 1.15
 
 require (
+	github.com/dhruvbehl/game-apis v0.0.0-20220209231545-d47bcbafe802
 	github.com/rs/zerolog v1.26.1
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	google.golang.org/genproto v0.0.0-20220208230804-65c12eb4c068 // indirect
 	google.golang.org/grpc v1.44.0
 )
--- internal/server/grpc/grpc.go
+++ internal/server/grpc/grpc.go
@@ -16,10 +16,17 @@ type Grpc struct {
 
 var HighScore = math.MaxFloat64
 
-func (grpc *Grpc) SetHighScore(ctx context.Context, input *pbhighscore.SetHighScoreRequest) (*pbhighscore.SetHighScoreResponse, error) {
+func (g *Grpc) SetHighScore(ctx context.Context, input *pbhighscore.SetHighScoreRequest) (*pbhighscore.SetHighScoreResponse, error) {
 	log.Info().Msg("SetHighScore in game-highscore")
 	HighScore = input.HighScore
 	return &pbhighscore.SetHighScoreResponse{
 		Set: true,
+	}, nil
+}
+
+func (g *Grpc) GetHighScore(ctx context.Context, input *pbhighscore.GetHighScoreRequest) (*pbhighscore.GetHighScoreResponse, error) {
+	log.Info().Msg("GetHighScore in game-highscore")
+	return &pbhighscore.GetHighScoreResponse{
+		HighScore: HighScore,
 	}, nil
 }
